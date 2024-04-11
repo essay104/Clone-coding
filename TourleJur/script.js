8d5bc655784eee59966f8edbbdc45b71a5bfd2af
essay104 <156299961+essay104@users.noreply.github.com>
2024-04-11 09:03:10 +0900

    Add files via upload

--- TourleJur/script.js
+++ TourleJur/script.js
@@ -1,6 +1,10 @@
 const aDeco = document.querySelectorAll("a");
+const headerToggle = document.querySelector(".header-toggle");
+const headerBottomA = document.querySelectorAll(".headerBottom a");
+const headerTop = document.querySelector(".headerTop");
+const headerBottom = document.querySelector(".headerBottom");
 
-//a태그에 mousehover했을때 스타일이 변하는 이벤트
+// a태그에 mousehover했을때 스타일이 변하는 이벤트
 
 aDeco.forEach((a) => {
   if (a.innerText != "Login") {
@@ -12,4 +16,24 @@ aDeco.forEach((a) => {
       a.style = "";
     });
   }
+});
+
+//toggle button 출현 이벤트
+
+headerBottomA.forEach((a) => {
+  a.addEventListener("mouseover", () => {
+    headerToggle.classList.add("toggle");
+    // headerToggle.style.height = "150px";
+  });
+  // a.addEventListener("mouseleave", () => {
+  //   headerToggle.classList.remove("toggle");
+  // });
+});
+
+headerToggle.addEventListener("mouseleave", () => {
+  headerToggle.classList.remove("toggle");
+});
+headerTop.addEventListener("mouseover", () => {
+  console.log("evnet");
+  headerToggle.classList.remove("toggle");
 });
