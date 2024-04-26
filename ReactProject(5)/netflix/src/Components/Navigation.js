46fc0f48e68435b04ce36ad68360c73ca2c6bf3c
essay104 <essay104@daum.net>
2024-04-26 18:30:39 +0900

    240426

--- ReactProject(5)/netflix/src/Components/Navigation.js
+++ ReactProject(5)/netflix/src/Components/Navigation.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import netflix from "../netflix.png";
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
-  )
-}
+    <div className="totalbar">
+      <div className="logo">
+        <img width={100} src={netflix} />
+        <ul className="nav">
+          <li className="nav-item">Movie</li>
+          <li className="nav-item">TV show</li>
+          <li className="nav-item">Animation</li>
+          <li className="nav-item">Drama</li>
+        </ul>
+      </div>
+      <form>
+        <input id="search-bar" type="search" placeholder="Search" />
+        <label for="serach-bar">Search</label>
+      </form>
+    </div>
+  );
+};
 
-export default Navigation
+export default Navigation;
