3112f6b576c525a7eca8ddb9acb511b5e6bddbaf
essay104 <156299961+essay104@users.noreply.github.com>
2024-04-28 21:25:07 +0900

    240428

--- HTML&CSS&JS(12)/fixedNews/script.js
+++ HTML&CSS&JS(12)/fixedNews/script.js
@@ -1,7 +1,30 @@
-//scroll btn을 누르면 최하단으로 이동하는 이벤트
-
 const btn = document.querySelector(".scroll-btn");
 const body = document.querySelector("body");
+const todayDate = document.querySelector(".today");
+const content01 = document.querySelector(".content01");
+const content02 = document.querySelector(".content02");
+const content03 = document.querySelector(".content03");
+const iframe = document.querySelector("iframe");
+
+//페이지이동
+
+const newsChange01 = () => {
+  iframe.setAttribute("src", "./index2.html");
+};
+
+const newsChange02 = () => {
+  iframe.setAttribute("src", "./index3.html");
+};
+
+const newsChange03 = () => {
+  iframe.setAttribute("src", "./index4.html");
+};
+
+content01.addEventListener("click", newsChange01);
+content02.addEventListener("click", newsChange02);
+content03.addEventListener("click", newsChange03);
+
+//scroll btn을 누르면 최하단으로 이동하는 이벤트
 
 const btnEventHandler = () => {
   window.scrollTo({
@@ -12,11 +35,6 @@ const btnEventHandler = () => {
 };
 
 btn.addEventListener("click", btnEventHandler);
-
-window.addEventListener("scroll", () => {
-  const scrollY = this.scrollY;
-  console.log(scrollY);
-});
 
 window.addEventListener("wheel", (e) => {
   if (window.innerHeight >= 900) {
@@ -29,4 +47,12 @@ window.addEventListener("wheel", (e) => {
   }
 });
 
-//toolbar Event
+//날짜표시 이벤트
+
+const newDay = new Date();
+
+const todayYear = newDay.getFullYear();
+const today = newDay.getMonth() + 1;
+const todayDay = newDay.getDate();
+
+todayDate.innerText = `${todayYear}년 ${today}월 ${todayDay}일`;
--- HTML&CSS&JS(12)/fullpage/script.js
+++ HTML&CSS&JS(12)/fullpage/script.js
@@ -9,6 +9,14 @@ new fullpage("#fullpage", {
 });
 
 const titleArrow = document.querySelector("#nav-icon");
+const arrows = document.querySelectorAll(".arrow");
+const imgs = ["img01.jpg", "img02.jpg", "img03.jpg"];
+const slider = document.querySelector(".event-slider");
+const img = document.querySelector(".content-img");
+const sc0 = document.querySelector("#sc0");
+console.log(sc0);
+
+console.log(slider);
 
 titleArrow.addEventListener("click", () => {
   if (!titleArrow.classList.contains("arrowActive")) {
@@ -17,3 +25,33 @@ titleArrow.addEventListener("click", () => {
     titleArrow.classList.remove("arrowActive");
   }
 });
+
+let imgSrc = `./img/sc01/${imgs[0]}`;
+img.src = imgSrc;
+sc0.style.background = `url(./img/sc01/${imgs[0]}`;
+
+let i = 0;
+
+arrows.forEach((arrow) => {
+  arrow.addEventListener("click", (e) => {
+    const direction = e.target.id === "left" ? "prev" : "next";
+    changeImg(direction);
+  });
+});
+
+changeImg = (direction) => {
+  console.log("click");
+  if (direction === "next") {
+    i++;
+    if (i >= imgs.length) {
+      i = 0;
+    }
+  } else if (direction === "prev") {
+    i--;
+    if (i < 0) {
+      i = img.length - 1;
+    }
+  }
+  img.src = `./img/sc01/${imgs[i]}`;
+  sc0.style.background = `url(./img/sc01/${imgs[i]})`;
+};
