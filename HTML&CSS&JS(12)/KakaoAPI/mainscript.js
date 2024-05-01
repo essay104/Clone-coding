8b1868fccc0f635e1cd50bc976f76ca70d1ace10
essay104 <essay104@daum.net>
2024-05-01 18:18:48 +0900

    240501

--- HTML&CSS&JS(12)/KakaoAPI/mainscript.js
+++ HTML&CSS&JS(12)/KakaoAPI/mainscript.js
@@ -1,6 +1,13 @@
 const markers = document.querySelectorAll(".marker");
 const modal = document.querySelector(".modal");
 const btn = document.querySelector(".modal-header button");
+const form = document.querySelector("form");
+const submit = document.querySelector(".info-submit");
+const storeName = document.querySelector(".modal-header h1");
+
+console.log(storeName);
+
+console.log(markers);
 
 markers.forEach((marker) => {
   marker.addEventListener("click", function () {
@@ -10,4 +17,23 @@ markers.forEach((marker) => {
 
 btn.addEventListener("click", () => {
   modal.style.display = "none";
+  storeName.innerText = "";
 });
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault;
+  const content = form.value;
+  console.log(content);
+});
+
+markers.forEach((marker) => {
+  marker.addEventListener("click", () => {
+    if ((marker.classList == "store01")) {
+      storeName.innerText = "함스브로제과점";
+    } 
+  });
+});
+
+const init = () => {
+  storeName.innerText = "";
+};
--- HTML&CSS&JS(12)/KakaoAPI/script.js
+++ HTML&CSS&JS(12)/KakaoAPI/script.js
@@ -2,7 +2,7 @@ var container = document.getElementById("map"); //지도를 담을 영역의 DOM
 var options = {
   //지도를 생성할 때 필요한 기본 옵션
   center: new kakao.maps.LatLng(37.6513398, 127.0362835), //지도의 중심좌표.
-  level: 5, //지도의 레벨(확대, 축소 정도)
+  level: 4, //지도의 레벨(확대, 축소 정도)
 };
 
 var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
