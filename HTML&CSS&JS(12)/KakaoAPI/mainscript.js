4abe0385bb74db11e4d4104ac1fb720e97168205
essay104 <essay104@daum.net>
2024-05-02 18:18:36 +0900

    240502

--- HTML&CSS&JS(12)/KakaoAPI/mainscript.js
+++ HTML&CSS&JS(12)/KakaoAPI/mainscript.js
@@ -4,8 +4,20 @@ const btn = document.querySelector(".modal-header button");
 const form = document.querySelector("form");
 const submit = document.querySelector(".info-submit");
 const storeName = document.querySelector(".modal-header h1");
+const modalImg = document.querySelector(".modal-img");
+const img = [
+  "img01.jpg",
+  "img02.jpg",
+  "img03.jpg",
+  "img04.jpg",
+  "img05.jpg",
+  "img06.jpg",
+  "img07.jpg",
+];
 
-console.log(storeName);
+console.log(modalImg);
+
+// console.log(storeName);
 
 console.log(markers);
 
@@ -17,7 +29,6 @@ markers.forEach((marker) => {
 
 btn.addEventListener("click", () => {
   modal.style.display = "none";
-  storeName.innerText = "";
 });
 
 form.addEventListener("submit", (e) => {
@@ -26,11 +37,48 @@ form.addEventListener("submit", (e) => {
   console.log(content);
 });
 
-markers.forEach((marker) => {
+// markers.forEach((marker, idx) => {
+//   marker.addEventListener("click", () => {
+//     if (idx == 0) {
+//       storeName.innerText = "함스브로제과점";
+//     } else if (idx == 1) {
+//       storeName.innerText = "옛가 칼국수"
+//     }
+//   });
+// });
+
+markers.forEach((marker, idx) => {
   marker.addEventListener("click", () => {
-    if ((marker.classList == "store01")) {
-      storeName.innerText = "함스브로제과점";
-    } 
+    switch (idx) {
+      case 0:
+        storeName.innerText = "함스브로제과점";
+        modalImg.style.backgroundImage = `url(./img/${img[0]})`;
+        break;
+      case 1:
+        storeName.innerText = "옛가 칼국수";
+        modalImg.style.backgroundImage = `url(./img/${img[1]})`;
+        break;
+      case 2:
+        storeName.innerText = "마쓰무라 돈까스";
+        modalImg.style.backgroundImage = `url(./img/${img[2]})`;
+        break;
+      case 3:
+        storeName.innerText = "장충 왕족발";
+        modalImg.style.backgroundImage = `url(./img/${img[3]})`;
+        break;
+      case 4:
+        storeName.innerText = "최고성";
+        modalImg.style.backgroundImage = `url(./img/${img[4]})`;
+        break;
+      case 5:
+        storeName.innerText = "뼈다귀 해장국";
+        modalImg.style.backgroundImage = `url(./img/${img[5]})`;
+        break;
+      case 6:
+        storeName.innerText = "소문난 보배곱창";
+        modalImg.style.backgroundImage = `url(./img/${img[6]})`;
+        break;
+    }
   });
 });
 
