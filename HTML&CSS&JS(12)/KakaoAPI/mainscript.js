4228af3b608eb74df1a4c7995c2cb3291d91d2e1
essay104 <essay104@daum.net>
2024-05-03 18:21:01 +0900

    240503

--- HTML&CSS&JS(12)/KakaoAPI/mainscript.js
+++ HTML&CSS&JS(12)/KakaoAPI/mainscript.js
@@ -1,7 +1,6 @@
 const markers = document.querySelectorAll(".marker");
 const modal = document.querySelector(".modal");
 const btn = document.querySelector(".modal-header button");
-const form = document.querySelector("form");
 const submit = document.querySelector(".info-submit");
 const storeName = document.querySelector(".modal-header h1");
 const modalImg = document.querySelector(".modal-img");
@@ -15,11 +14,7 @@ const img = [
   "img07.jpg",
 ];
 
-console.log(modalImg);
-
 // console.log(storeName);
-
-console.log(markers);
 
 markers.forEach((marker) => {
   marker.addEventListener("click", function () {
@@ -30,22 +25,6 @@ markers.forEach((marker) => {
 btn.addEventListener("click", () => {
   modal.style.display = "none";
 });
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault;
-  const content = form.value;
-  console.log(content);
-});
-
-// markers.forEach((marker, idx) => {
-//   marker.addEventListener("click", () => {
-//     if (idx == 0) {
-//       storeName.innerText = "함스브로제과점";
-//     } else if (idx == 1) {
-//       storeName.innerText = "옛가 칼국수"
-//     }
-//   });
-// });
 
 markers.forEach((marker, idx) => {
   marker.addEventListener("click", () => {
@@ -85,3 +64,63 @@ markers.forEach((marker, idx) => {
 const init = () => {
   storeName.innerText = "";
 };
+
+//accodion Events
+
+const oppener = document.querySelector(".select-opener");
+const accodion = document.querySelector(".select-accodion");
+const infos = document.querySelectorAll(".fa-info");
+const locations = document.querySelectorAll(".fa-location-dot");
+
+console.log(infos);
+
+console.log(accodion);
+
+oppener.addEventListener("click", () => {
+  oppener.classList.toggle("active");
+  accodion.style.display = "block";
+  // if (accodion.style.display === "block") {
+  //   oppener.classList.toggle("acitve");
+  //   accodion.style.display = "none";
+  // }
+});
+
+infos.forEach((info, idx) => {
+  info.addEventListener("click", () => {
+    modal.style.display = "block";
+    switch (idx) {
+      case 0:
+        storeName.innerText = "함스브로제과점";
+        modalImg.style.backgroundImage = `url(./img/${img[0]})`;
+        break;
+      case 1:
+        storeName.innerText = "옛가 칼국수";
+        modalImg.style.backgroundImage = `url(./img/${img[1]})`;
+        break;
+      case 2:
+        storeName.innerText = "마쓰무라 돈까스";
+        modalImg.style.backgroundImage = `url(./img/${img[2]})`;
+        break;
+      case 3:
+        storeName.innerText = "장충 왕족발";
+        modalImg.style.backgroundImage = `url(./img/${img[3]})`;
+        break;
+      case 4:
+        storeName.innerText = "최고성";
+        modalImg.style.backgroundImage = `url(./img/${img[4]})`;
+        break;
+      case 5:
+        storeName.innerText = "뼈다귀 해장국";
+        modalImg.style.backgroundImage = `url(./img/${img[5]})`;
+        break;
+      case 6:
+        storeName.innerText = "소문난 보배곱창";
+        modalImg.style.backgroundImage = `url(./img/${img[6]})`;
+        break;
+    }
+  });
+});
+
+locations.addEventListener("click", () => {
+  
+});
