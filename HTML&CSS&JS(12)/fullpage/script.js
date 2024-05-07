d855d5f2ce91595e672a3375b03b6448964882a1
essay104 <156299961+essay104@users.noreply.github.com>
2024-05-07 21:42:41 +0900

    240507

--- HTML&CSS&JS(12)/fullpage/script.js
+++ HTML&CSS&JS(12)/fullpage/script.js
@@ -10,11 +10,15 @@ new fullpage("#fullpage", {
 
 const titleArrow = document.querySelector("#nav-icon");
 const arrows = document.querySelectorAll(".arrow");
-const imgs = ["img01.jpg", "img02.jpg", "img03.jpg"];
 const sliders = document.querySelectorAll(".event-slider");
 const img = document.querySelector(".content-img");
+const sections = ["sc0", "sc1", "sc2", "sc3"];
+const imgs = ["img01.jpg", "img02.jpg", "img03.jpg"];
 const sc0 = document.querySelector("#sc0");
 const sc1 = document.querySelector("#sc1");
+const arrowsNumbers = document.querySelectorAll(".arrowsNumber");
+
+console.log(arrows);
 
 titleArrow.addEventListener("click", () => {
   if (!titleArrow.classList.contains("arrowActive")) {
@@ -24,21 +28,16 @@ titleArrow.addEventListener("click", () => {
   }
 });
 
-let imgSrc = `./img/sc01/${imgs[0]}`;
-let imgSrc2 = `./img/sc02/${imgs[0]}`;
+const src = document.createAttribute("src");
+src.value = `./img/sc0/${imgs[0]}`;
+
+let imgSrc = `./img/sc0/${imgs[0]}`;
 
 img.src = imgSrc;
-sc0.style.backgroundImage = `url(./img/sc01/${imgs[0]}`;
-sc1.style.backgroundImage = `url(./img/sc02/${imgs[0]}`;
+sc0.style.backgroundImage = `url(./img/sc0/${imgs[0]}`;
+sc1.style.backgroundImage = `url(./img/sc1/${imgs[0]}`;
 
 let i = 0;
-
-sliders.forEach((slider, idx) => {
-  if (idx == 1) {
-    const img = slider.querySelector("img");
-    img.src = `./img/sc02/${imgs[0]}`;
-  }
-});
 
 arrows.forEach((arrow) => {
   arrow.addEventListener("click", (e) => {
@@ -60,8 +59,9 @@ changeImg = (direction) => {
       i = img.length - 1;
     }
   }
-  img.src = `./img/sc01/${imgs[i]}`;
 
-  sc0.style.backgroundImage = `url(./img/sc01/${imgs[i]})`;
-  sc1.style.backgroundImage = `url(./img/sc02/${imgs[i]})`;
+  imgSrc = `./img/sc0/${imgs[i]}`;
+
+  sc0.style.backgroundImage = `url(./img/sc0/${imgs[0]}`;
+  sc1.style.backgroundImage = `url(./img/sc1/${imgs[0]}`;
 };
