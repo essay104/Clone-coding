5b22ac78dcc48c4e3b0f6ec35ea82de48d723966
essay104 <156299961+essay104@users.noreply.github.com>
2024-05-11 19:30:14 +0900

    240511

--- clone(3)/SBS(HTML&CSS&JS)/script.js
+++ clone(3)/SBS(HTML&CSS&JS)/script.js
@@ -3,7 +3,6 @@ const navBar = document.querySelector(".toggle-nav a");
 const navBarMenu = document.querySelector(".toggle-nav ul");
 const navBarTitle = document.querySelector(".toggle-nav-title");
 const navBarAngle = document.querySelector(".fa-angle-down");
-console.log(navBar, navBarMenu);
 
 navBar.addEventListener("mouseenter", () => {
   console.log("enter");
@@ -30,3 +29,30 @@ window.addEventListener("click", (e) => {
     navBarAngle.style.transform = "rotate(0deg)";
   }
 });
+
+//slider Event
+
+const slider = document.querySelector(".main-slider-container");
+const slides = document.querySelectorAll(".main-slider-container li");
+const prev01 = document.querySelector(".prev");
+const next01 = document.querySelector(".next");
+
+let currentIdx = 0;
+
+const slideWidth = 1140;
+
+const cloneSlides = () => {
+  for (let i = 0; i < slides.length; i++) {
+    let clone = slides[i].cloneNode(true);
+    clone.classList.add("clone");
+    slider.appendChild(clone);
+  }
+
+  for (let i = slides.length - 1; i >= 0; i--) {
+    let clone = slides[i].cloneNode(true);
+    clone.classList.add("clone");
+    slider.prepend(clone);
+  }
+};
+
+cloneSlides();
