f51d612a13c4c1a4da1a0e44ed0a84e429d2ba31
essay104 <156299961+essay104@users.noreply.github.com>
2024-05-11 15:06:39 +0900

    240511

--- clone(3)/SBS(HTML&CSS&JS)/script.js
+++ clone(3)/SBS(HTML&CSS&JS)/script.js
@@ -1,14 +1,32 @@
 const nav = document.querySelector(".toggle-nav");
 const navBar = document.querySelector(".toggle-nav a");
 const navBarMenu = document.querySelector(".toggle-nav ul");
+const navBarTitle = document.querySelector(".toggle-nav-title");
+const navBarAngle = document.querySelector(".fa-angle-down");
 console.log(navBar, navBarMenu);
 
 navBar.addEventListener("mouseenter", () => {
   console.log("enter");
   navBarMenu.style.display = "block";
+  navBarTitle.style.color = "#f47499";
+  navBarTitle.style.textDecoration = "underline";
+  navBarTitle.style.transition = "all 0.5s";
+  navBarAngle.style.transform = "rotate(180deg)";
 });
 
 navBarMenu.addEventListener("mouseleave", () => {
   console.log("enter");
   navBarMenu.style.display = "none";
+  navBarTitle.style.color = "#000";
+  navBarTitle.style.textDecoration = "none";
+  navBarAngle.style.transform = "rotate(0deg)";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target !== navBarMenu) {
+    navBarMenu.style.display = "none";
+    navBarTitle.style.color = "#000";
+    navBarTitle.style.textDecoration = "none";
+    navBarAngle.style.transform = "rotate(0deg)";
+  }
 });
